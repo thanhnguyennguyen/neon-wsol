54c8599e99e74726957a95a342be36af93de38b2
thanhnguyennguyen <thanhnguyennguyen@users.noreply.github.com>
2023-12-05 21:10:53 +0700

    feat: initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,194 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from "dotenv";
+import "hardhat-deploy";
+import type { HardhatUserConfig } from "hardhat/config";
+import type { NetworkUserConfig } from "hardhat/types";
+import { resolve } from "path";
+
+import "./tasks/accounts";
+import "./tasks/greet";
+import "./tasks/taskDeploy";
+
+const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
+dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
+
+// Ensure that we have all the environment variables we need.
+const mnemonic: string | undefined = process.env.MNEMONIC;
+if (!mnemonic) {
+  throw new Error("Please set your MNEMONIC in a .env file");
+}
+
+const infuraApiKey: string | undefined = process.env.INFURA_API_KEY;
+if (!infuraApiKey) {
+  throw new Error("Please set your INFURA_API_KEY in a .env file");
+}
+
+const chainIds = {
+  "arbitrum-mainnet": 42161,
+  avalanche: 43114,
+  bsc: 56,
+  ganache: 1337,
+  hardhat: 31337,
+  mainnet: 1,
+  "optimism-mainnet": 10,
+  "polygon-mainnet": 137,
+  "polygon-mumbai": 80001,
+  sepolia: 11155111,
+  viction: 88,
+  victiontestnet: 89,
+  neonevm: 245022934,
+  neondev: 245022926,
+};
+
+function getChainConfig(chain: keyof typeof chainIds): NetworkUserConfig {
+  let jsonRpcUrl: string;
+  switch (chain) {
+    case "avalanche":
+      jsonRpcUrl = "https://api.avax.network/ext/bc/C/rpc";
+      break;
+    case "bsc":
+      jsonRpcUrl = "https://bsc-dataseed1.binance.org";
+      break;
+    case "viction":
+      jsonRpcUrl = "https://rpc.viction.xyz";
+      break;
+    case "victiontestnet":
+      jsonRpcUrl = "https://rpc-testnet.viction.xyz";
+      break;
+    case "neonevm":
+      jsonRpcUrl = "https://neon-proxy-mainnet.solana.p2p.org"
+      break;
+    case "neondev":
+      jsonRpcUrl = "https://devnet.neonevm.org"
+      break;
+    default:
+      jsonRpcUrl = "https://" + chain + ".infura.io/v3/" + infuraApiKey;
+  }
+  return {
+    accounts: {
+      count: 10,
+      mnemonic,
+      path: "m/44'/60'/0'/0",
+    },
+    chainId: chainIds[chain],
+    url: jsonRpcUrl,
+  };
+}
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  namedAccounts: {
+    deployer: 0,
+  },
+  etherscan: {
+    apiKey: {
+      arbitrumOne: process.env.ARBISCAN_API_KEY || "",
+      avalanche: process.env.SNOWTRACE_API_KEY || "",
+      bsc: process.env.BSCSCAN_API_KEY || "",
+      mainnet: process.env.ETHERSCAN_API_KEY || "",
+      optimisticEthereum: process.env.OPTIMISM_API_KEY || "",
+      polygon: process.env.POLYGONSCAN_API_KEY || "",
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
+      viction: process.env.VICSCAN_API_KEY || "",
+      victiontestnet: process.env.VICSCAN_TESTNET_API_KEY || "",
+      neondev: "",
+      neonevm: "",
+    },
+    customChains: [
+      {
+        network: "neondev",
+        chainId: 245022926,
+        urls: {
+          apiURL: "https://devnet-api.neonscan.org/hardhat/verify",
+          browserURL: "https://devnet.neonscan.org",
+        },
+      },
+      {
+        network: "neonevm",
+        chainId: 245022934,
+        urls: {
+          apiURL: "https://api.neonscan.org/hardhat/verify",
+          browserURL: "https://neonscan.org",
+        },
+      },
+      {
+        network: "viction",
+        chainId: 88,
+        urls: {
+          apiURL: "https://www.vicscan.xyz/api/contract/hardhat/verify",
+          browserURL: "https://www.vicscan.xyz"
+        }
+      },
+      {
+        network: "victiontestnet",
+        chainId: 89,
+        urls: {
+          apiURL: "https://scan-api-testnet.viction.xyz/api/contract/hardhat/verify",
+          browserURL: "https://www.testnet.vicscan.xyz"
+        }
+      }
+    ]
+  },
+  gasReporter: {
+    currency: "USD",
+    enabled: process.env.REPORT_GAS ? true : false,
+    excludeContracts: [],
+    src: "./contracts",
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic,
+      },
+      chainId: chainIds.hardhat,
+    },
+    ganache: {
+      accounts: {
+        mnemonic,
+      },
+      chainId: chainIds.ganache,
+      url: "http://localhost:8545",
+    },
+    arbitrum: getChainConfig("arbitrum-mainnet"),
+    avalanche: getChainConfig("avalanche"),
+    bsc: getChainConfig("bsc"),
+    mainnet: getChainConfig("mainnet"),
+    optimism: getChainConfig("optimism-mainnet"),
+    "polygon-mainnet": getChainConfig("polygon-mainnet"),
+    "polygon-mumbai": getChainConfig("polygon-mumbai"),
+    sepolia: getChainConfig("sepolia"),
+    viction: getChainConfig("viction"),
+    victiontestnet: getChainConfig("victiontestnet"),
+    neonevm: getChainConfig("neonevm"),
+    neondev: getChainConfig("neondev"),
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      metadata: {
+        // Not including the metadata hash
+        // https://github.com/paulrberg/hardhat-template/issues/31
+        bytecodeHash: "none",
+      },
+      // Disable the optimizer when debugging
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+    },
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v6",
+  },
+};
+
+export default config;
